52c6656ffda2d7e73101eeb1d852130b1c5bedfb
Murali Nutalapati <info@muraliprashanth.me>
2017-07-17 13:24:53 +0530

    updates the delete post reducer

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions';
+import {fetchPost, deletePost} from '../actions';
 import {Link} from 'react-router-dom';
 
 
 class PostsShow extends Component {
 
     componentDidMount() {
+        if(!this.props.post){
+            const {id} = this.props.match.params;
+            this.props.fetchPost(id);
+        }
+    }
+
+    onDeleteClick() {
         const {id} = this.props.match.params;
-        this.props.fetchPost(id);
+        this.props.deletePost(id, () => {
+            this.props.history.push("/");
+        });
     }
 
     render() {
@@ -18,6 +27,10 @@ class PostsShow extends Component {
             return (
                 <div>
                     <Link to="/">Back to Posts</Link>
+                    <button className="btn btn-danger pull-xs-right"
+                            onClick={this.onDeleteClick.bind(this)}
+
+                    >Delete Post</button>
                     <h3>{post.title}</h3>
                     <h6>{post.categories}</h6>
                     <div>{post.content}</div>
@@ -36,4 +49,4 @@ function mapStateToProps({posts}, ownProps) {
     return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
